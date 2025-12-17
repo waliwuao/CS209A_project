8cbc857d263b03a335ab82cbbee70d454dce6034
musH0514 <mushyl_1801029@qq.com>
2025-12-17 22:23:22 +0800

    transfer

--- src/main/java/com/example/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface QuestionRepository extends JpaRepository<QuestionItem, Integer>{
-    List<QuestionItem> findAll();
-}
